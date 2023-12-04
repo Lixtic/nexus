cdfc17740bf909e056098ba10c6f7bd6857390da
Brian Yu <brian-yu-nexusflow@users.noreply.huggingface.co>
2023-12-04 18:21:12 +0000

    Update constants.py

--- constants.py
+++ constants.py
@@ -71,3 +71,9 @@ HEADER_HTML = """<img width="50" height="50" style="float:left; margin: 0px;" sr
 GMAPS_EMBED_HTML_TEMPLATE = """
 <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q={location}+{address}&amp;t=&amp;z=18&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
 """
+
+CHANGE_DARK_SCRIPT = """
+() => {
+    document.body.classList.toggle('dark');
+}
+"""
