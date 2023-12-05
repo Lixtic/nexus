6db0fcc427976fbb79419e58ac66c3979436d79d
Brian Yu <brian-yu-nexusflow@users.noreply.huggingface.co>
2023-12-05 01:30:40 +0000

    Update tools.py

--- tools.py
+++ tools.py
@@ -2,8 +2,10 @@
 These are all the tools used in the NexusRaven V2 demo! You can provide any tools you want to Raven.
 
 Nothing in this file is specific to Raven, code/information related to Raven can be found in the `raven_demo.py` file.
+
+For more information about the Google Maps Places API Python client, see https://github.com/googlemaps/google-maps-services-python
 """
-from typing import Dict, List, Union
+from typing import Dict, List
 
 from math import radians, cos, sin, asin, sqrt
 
@@ -22,6 +24,12 @@ class Tools:
 
         self.gmaps = Client(config.gmaps_client_key)
         self.client_ip: str | None = None
+
+    def capitalize(self, s: str) -> str:
+        if s.lower() != s:
+            return s
+
+        return s.title()
 
     def haversine(self, lon1, lat1, lon2, lat2) -> float:
         """
@@ -52,7 +60,9 @@ class Tools:
         except:
             return "San Francisco, California, US"
 
-    def sort_results(self, places : list, sort: str, descending: bool = True, first_n : int = None) -> List:
+    def sort_results(
+        self, places: list, sort: str, descending: bool = True, first_n: int = None
+    ) -> List:
         """
         Sorts the results by either 'distance', 'rating' or 'price'.
 
@@ -60,7 +70,7 @@ class Tools:
         - sort (str): If set, sorts by either 'distance' or 'rating' or 'price'. ONLY supports 'distance' or 'rating' or 'price'.
         - descending (bool): If descending is set, setting this boolean to true will sort the results such that the highest values are first.
         - first_n (int): If provided, only retains the first n items in the final sorted list.
-        
+
         When people ask for 'closest' or 'nearest', sort by 'distance'.
         When people ask for 'cheapest' or 'most expensive', sort by 'price'.
         When people ask for 'best' or 'highest rated', sort by rating.
@@ -88,7 +98,13 @@ class Tools:
 
         - location: This can be a city like 'Austin', or a place like 'Austin Airport', etc.
         """
-        return self.gmaps.geocode(location)
+
+        # For response content, see https://developers.google.com/maps/documentation/geocoding/requests-geocoding#GeocodingResponses
+        results = self.gmaps.geocode(location)
+        location = self.capitalize(location)
+        for r in results:
+            r["name"] = location
+        return results
 
     def get_distance(self, place_1: str, place_2: str):
         """
@@ -101,30 +117,41 @@ class Tools:
         """
         if isinstance(place_1, list) and len(place_1) > 0:
             place_1 = place_1[0]
-
         if isinstance(place_2, list) and len(place_2) > 0:
             place_2 = place_2[0]
 
-        latlong_1 = self.get_latitude_longitude(place_1)
-        latlong_2 = self.get_latitude_longitude(place_2)
-
         if isinstance(place_1, dict):
-            place_1 = place_1["name"]
+            place_1: str = place_1["name"]
         if isinstance(place_2, dict):
-            place_2 = place_2["name"]
+            place_2: str = place_2["name"]
 
-        if len(latlong_1) == 0 or len(latlong_2) == 0:
-            return "No place found for the query. Please be more explicit."
+        latlong_1 = self.get_latitude_longitude(place_1)
+        if len(latlong_1) == 0:
+            return f"No place found for `{place_1}`. Please be more explicit."
 
-        latlong1 = latlong_1[0]["geometry"]["location"]
-        latlong2 = latlong_2[0]["geometry"]["location"]
+        latlong_2 = self.get_latitude_longitude(place_2)
+        if len(latlong_2) == 0:
+            return f"No place found for `{place_2}`. Please be more explicit."
+
+        latlong_1 = latlong_1[0]
+        latlong_2 = latlong_2[0]
+
+        latlong_values_1 = latlong_1["geometry"]["location"]
+        latlong_values_2 = latlong_2["geometry"]["location"]
 
         dist = self.haversine(
-            latlong1["lng"], latlong1["lat"], latlong2["lng"], latlong2["lat"]
+            latlong_values_1["lng"],
+            latlong_values_1["lat"],
+            latlong_values_2["lng"],
+            latlong_values_2["lat"],
         )
         dist = dist * 0.621371
 
-        return [f"The distance between {place_1} and {place_2} is {dist:.3f} miles"]
+        return [
+            latlong_1,
+            latlong_2,
+            f"The distance between {place_1} and {place_2} is {dist:.3f} miles",
+        ]
 
     def get_recommendations(self, topics: list, lat_long: tuple):
         """
@@ -138,6 +165,7 @@ class Tools:
 
         topic = " ".join(topics)
         latlong = lat_long[0]["geometry"]["location"]
+        # For response format, see https://developers.google.com/maps/documentation/places/web-service/search-find-place#find-place-responses
         results = self.gmaps.places(
             query=topic,
             location=latlong,
@@ -165,6 +193,7 @@ class Tools:
 
         type_of_place = " ".join(type_of_place)
         # Perform the search using Google Places API
+        # For response format, see https://developers.google.com/maps/documentation/places/web-service/search-nearby#nearby-search-responses
         places_result = self.gmaps.places_nearby(
             location=location, keyword=type_of_place, radius=radius_miles * 1609.34
         )
@@ -181,7 +210,8 @@ class Tools:
             if distance == 0.0:
                 continue
 
-            place["distance"] = f"{distance} kilometers from {verb_location}"
+            distance = distance * 0.621371
+            place["distance"] = f"{distance} miles from {verb_location}"
             new_places.append(place)
 
         places = new_places
@@ -212,18 +242,26 @@ class Tools:
                     location = None
                 if location and isinstance(location, str):
                     place_name += " , " + location
-            elif isinstance(place_name, dict) and "results" in place_name and "name" in place_name["results"]:
+            elif (
+                isinstance(place_name, dict)
+                and "results" in place_name
+                and "name" in place_name["results"]
+            ):
                 place_name = place_name["results"]["name"]
             elif isinstance(place_name, dict) and "name" in place_name:
                 place_name = place_name["name"]
 
-            search_results = self.gmaps.places(place_name)
+            # For response format, see https://developers.google.com/maps/documentation/places/web-service/search-find-place#find-place-responses
+            search_results = self.gmaps.find_place(
+                place_name, input_type="textquery", location_bias="ipbias"
+            )
 
-            if not search_results.get("results"):
+            if len(search_results.get("candidates", [])) == 0:
                 return []
 
             # Assuming the first result is the most relevant
-            place_id = search_results["results"][0]["place_id"]
+            place_id = search_results["candidates"][0]["place_id"]
+            # For response format, see https://developers.google.com/maps/documentation/places/web-service/details#PlaceDetailsResponses
             place_details = self.gmaps.place(place_id=place_id)
             reviews = place_details["result"].get("reviews", [])
 
